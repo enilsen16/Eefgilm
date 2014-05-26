f13d4d10fa9ff9e66eeaadbfa49eba17d3875d50
Erik Nilsen <enilsen16@live.com>
2014-05-26 10:11:22 -0700

    created gemfile_test.rb

--- test/features/gemfile_test.rb
+++ test/features/gemfile_test.rb
@@ -1,3 +1,13 @@
 require "test_helper"
 
 #Is a gemfile present?
+Describe "gemfile properties" do
+  feature "Is there a gemfile present?" do
+    #It should navigate to correct path
+
+    #look for Gemfile
+
+    #confirm that Gemfile is there
+
+  end
+end
