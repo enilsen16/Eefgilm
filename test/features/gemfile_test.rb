d2accf0d2f06642fe17bd66d825daf4b79859f98
Erik Nilsen <enilsen16@live.com>
2014-05-27 12:32:10 -0700

    Tests currently pass

--- test/features/gemfile_test.rb
+++ test/features/gemfile_test.rb
@@ -1,8 +1,20 @@
 require "minitest/autorun"
+require "eefgilm"
 
- describe "The Gemfile behaves correctly" do
+ describe "Gemfile" do
   it "The Gemfile can be read correctly" do
-    gemfile = Contents.new
-    gemfile.must_include "source 'https://rubygems.org'"
+    gemfile = Eefgilm::Gemfile.new
+    contentarray= gemfile.read
+    source = "source 'https://rubygems.org'"
+    contentarray.first.chomp.must_equal source
   end
+  it "checks to see if a gemfile is present" do
+    gemfile = File.exist?(File.join(File.dirname(__FILE__), "../data/default_gemfile/Gemfile"))
+    gemfile.must_equal true
+  end
+  # it "does not have a gemfile" do
+  #   gemfile = Eefgilm::Gemfile.new(no_gemfile)
+  #   check = gemfile.exists?
+  #   check.must_equal false
+  # end
 end
